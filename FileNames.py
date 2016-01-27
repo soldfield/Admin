7777d1f83de2293a14460d5fb017e0d783a6c52b
Simon Oldfield <soldfield@users.noreply.github.com>
2016-01-27 23:02:47 +0000

    Fn for adding file extensions to a whole folder

--- FileNames.py
+++ FileNames.py
@@ -2,31 +2,24 @@
 """
 Created on Thu Jan 21 09:36:46 2016
 
-
-
-
+Various functions for basic administrative tasks.
 
 @author: ee10sjo
 """
 
-
-
 from os import listdir
-from os.path import join
 import os
 
-dir_name = "Y:\\ee10sjo\\Oldfields_PhD\\4_Applications\\Python\\SurfaceAttributeAnalysis\\AttributeGrids\\"
+def extn_add(dir, extn):
+    """
+    Function to add a file extension to all files within a provided directory.
+    """
 
-listdir(dir_name)[0:17]
-
-extn = ".zmap"
-
-for i in listdir(dir_name)[0:17]:
-    newfn = os.path.join(dir_name, i + extn)
-    
-    old = dir_name + i
-    os.rename()
-    
-result = listdir(dir_name)
-print(result)
+    for f in listdir(dir):
+        new_fn = os.path.join(dir, f + extn)
+        old_fn = os.path.join(dir, f)
+        os.rename(old_fn, new_fn)
+        
+    result = listdir(dir)
+    print(result)
 
