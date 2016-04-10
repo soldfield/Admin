9d29462373ed14d50064a96f4d253149e01a079a
Simon Oldfield <soldfield@users.noreply.github.com>
2016-04-10 20:41:42 +0100

    Minimum viable product, assembles data from las files.

--- LAS_LocationExtract.py
+++ LAS_LocationExtract.py
@@ -7,25 +7,7 @@ table.
 
 @author: Simon J Oldfield
 
-
-   Copyright 2015 Simon J. Oldfield
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-   
-   
 """
-
-
 
 # MVP - Script that extracts location data from a series of las files
 # result shoudl be assembled ina form that can later be manipulated
@@ -40,17 +22,14 @@ import lasio
 dir = os.getcwd()
 las_ls = glob.glob("*.las")
 
-for i in las_ls:
-    lasio.read(i)
+
+for i in range(len(las_ls)):
+    d = lasio.read(las_ls[0])
+    uwi = d.well["UWI"].value
+    wlnam = d.well["WELL-NAME"].value
+    fldnam = d.well["FIELD-NAME"].value
+    long = str(d.well["LONG"].value)
+    lati = str(d.well["LATI"].value)
+    print(uwi + ', ' + wlnam + ', ' + fldnam  + ', ' +  long  + ', ' +  lati)
 
 
-# from lasio header extract, pull well name, field name, UWI, LATI, LONG
-
-DesHdrs = ("UWI", "WELL-NAME", "FIELD-NAME", "LONG", "LATI")
-
-# Loop list of desired headers to build database
-for i in DesHdrs:
-    l.well[i].value
-
-
-#return d
