6f92ce79d2c73d2a483e3432e477b63dee4e524b
Simon Oldfield <soldfield@users.noreply.github.com>
2016-04-11 14:30:54 +0100

    MVP acheived.  Mystery error related to nonetype on some one file.

--- LAS_LocationExtract.py
+++ LAS_LocationExtract.py
@@ -10,7 +10,7 @@ table.
 """
 
 # MVP - Script that extracts location data from a series of las files
-# result shoudl be assembled ina form that can later be manipulated
+# result should be assembled in a form that can later be manipulated
 # in a spreadsheet.
 
 import os
@@ -19,20 +19,31 @@ import lasio
 
 
 # identify las files in directory
-dir = os.getcwd()
+dir = 'Y:\\ee10sjo\\Oldfields_PhD\\3_Data\\ECO_Data_07Sep2015\\Wells_LASLogs_Selected_SO'
+os.chdir(dir)
 las_ls = glob.glob("*.las")
 
 def las_hdr_extract(las_ls):
+    #Open file for storing results.  
+    f = open('./test', 'w')
+    
+    # Loop through header extraction.
     for i in range(len(las_ls)):
-        d = lasio.read(las_ls[0])
+        d = lasio.read(las_ls[i])
         uwi = d.well["UWI"].value
         wlnam = d.well["WELL-NAME"].value
         fldnam = d.well["FIELD-NAME"].value
         long = str(d.well["LONG"].value)
         lati = str(d.well["LATI"].value)
-        print(uwi + ', ' + wlnam + ', ' + fldnam  + ', ' +  long  + ', ' +  lati)
+        output = uwi + ', ' + str(wlnam) + ', ' + str(fldnam) + ', ' +  str(long) + ', ' +  str(lati) + '\n'
+        try:
+            f.write(output)
+        except:
+            print(las_ls[i])
+            continue
+        
+        #return uwi, wlnam, fldnam, long, lati # Legacy code
+        
+    # close file leaving it ready for use
+    f.close()
 
-
-f1=open('./testfile', 'w+')
-f1.write(las_hdr_extract(las_ls))
-f1.close()
